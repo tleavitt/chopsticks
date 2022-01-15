c59e6d05b14f172864db09c1f9bd6bdc55d8b11c
tucker_leavitt <tucker.leavitt@gmail.com>
2022-01-15 16:23:12 -0300

    Looks pretty good!

--- server.go
+++ server.go
@@ -5,11 +5,28 @@ import (
     "fmt"
     "log"
     "net/http"
+    "io/ioutil"
+    "github.com/gorilla/mux"
 )
+
+func getImageRequestHandler(path string) http.Handler {
+    fn := func (w http.ResponseWriter, r *http.Request) {
+        fmt.Printf("serving image", r)
+        fileBytes, err := ioutil.ReadFile(path)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Printf("num fyle bytes: %d", len(fileBytes))
+        w.WriteHeader(http.StatusOK)
+        w.Header().Set("Content-Type", "application/octet-stream")
+        w.Write(fileBytes)
+    }
+    return http.HandlerFunc(fn)
+}
 
 func getHomeHandler(_ *gameState) http.Handler {
     fn := func (w http.ResponseWriter, r *http.Request) {
-        fmt.Printf("serving request %+v", r)
+        fmt.Printf("serving request %+v\n", r)
         body, err := os.ReadFile("frontend/index.html")
         if err != nil {
             log.Fatalf("Error when serving index.html, Err: %s", err)
@@ -27,7 +44,10 @@ func getMoveHandler(solveMap map[gameState]*playNode) http.Handler {
 }
 
 func serve(initGs *gameState, solveMap map[gameState]*playNode) {
-    http.Handle("/", getHomeHandler(initGs))
-    http.Handle("/move", getMoveHandler(solveMap))
+    r := mux.NewRouter()
+    r.Handle("/", getHomeHandler(initGs))
+    r.Handle("/static/hands.png", getImageRequestHandler("./frontend/static/hands.png"))
+    r.Handle("/move", getMoveHandler(solveMap))
+    http.Handle("/", r)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
