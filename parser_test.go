a64b3d2bae15be1a6c0658ac453b91c7eccd211a
tucker_leavitt <tucker.leavitt@gmail.com>
2022-01-15 23:20:07 -0300

    Front end selection working

--- parser_test.go
+++ parser_test.go
@@ -3,13 +3,28 @@ package main
 import (
   "fmt"
   "testing"
+  "encoding/json"
 )
 
-func TestUnmarshalMove(t *testing.T) {
-  fmt.Println("starting TestStateCopy")
+func TestUnmarshalState(t *testing.T) {
+  fmt.Println("starting TestUnmarshalState")
   j := `{"p1":{"lh":1,"rh":1},"p2":{"lh":1,"rh":1},"turn":"p1"}`
   _, err := parseUiMove([]byte(j))
   if err != nil {
     t.Fatal(err.Error())
   }
+}
+
+func TestMarshalState(t *testing.T) {
+  fmt.Println("starting TestMarshalState")
+  nextStateAndMove := &NextStateAndMove {
+    *initGame(),
+    Move{Left, Right,},
+  }
+
+  jsonResp, err := json.Marshal(nextStateAndMove) 
+  if err != nil {
+    t.Fatal(err.Error())
+  }
+  fmt.Printf("Serialized as %s", string(jsonResp))
 }
